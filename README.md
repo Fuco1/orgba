2ba2df9cdbaa4dc5e317591da2dc2a5d8232e610
Matus Goljer <matus.goljer@gmail.com>
2018-09-08 16:11:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,175 @@
+# org better api
+
+Better API for org.  It's that simple.  Org Mode has really confusing
+and fragmented API where many simple things are impossibly difficuly.
+This is the result of its organic growth.
+
+This package provides some high-level abstractions for common problems
+people want to solve.
+
+<!-- markdown-toc start - Don't edit this section. Run M-x markdown-toc-generate-toc again -->
+**Table of Contents**
+
+- [org better api](#org-better-api)
+- [Naming conventions](#naming-conventions)
+- [drawers](#drawers)
+    - [orgba-get-drawer `(name &optional use-children)`](#orgba-get-drawer-name-optional-use-children)
+    - [orgba-get-drawer-create `(name &optional use-children)`](#orgba-get-drawer-create-name-optional-use-children)
+- [tables](#tables)
+    - [orgba-insert-table `(&rest columns)`](#orgba-insert-table-rest-columns)
+    - [orgba-append-row `(row-data)`](#orgba-append-row-row-data)
+    - [orgba-lisp-to-table `(table-data)`](#orgba-lisp-to-table-table-data)
+- [headings](#headings)
+    - [orgba-next-heading](#orgba-next-heading)
+    - [orgba-heading-at `(&optional point)`](#orgba-heading-at-optional-point)
+    - [orgba-heading-title-at `(&optional point)`](#orgba-heading-title-at-optional-point)
+- [search](#search)
+- [time and dates](#time-and-dates)
+    - [orgba-time-as-timestamp `(&optional time active)`](#orgba-time-as-timestamp-optional-time-active)
+- [agenda](#agenda)
+    - [orgba-agenda-is-task-p](#orgba-agenda-is-task-p)
+- [misc](#misc)
+    - [orgba-restricted-p](#orgba-restricted-p)
+
+<!-- markdown-toc end -->
+
+
+# Naming conventions
+
+- Functions which query for a specific element or property etc. are
+  generally named `orgba-<thing>-at` and accept `point` as optional
+  argument to do the query at that point.  Otherwise default to
+  current `(point)`.
+- Functions which query for a specific element in the scope of the
+  current header are generally named `orgba-get-<thing>`.
+- Use `heading` consistently instead of `entry`, `task`, `headline`, `heading`...
+
+# Non-interactive APIs
+
+## properties
+
+### orgba-get-property `(name &optional dont-inherit)`
+
+Get property `name` of current heading.
+
+If `dont-inherit` is non-nil, do not use property inheritance.
+
+### orgba-get-property-create `(name value &optional dont-inherit)`
+
+Get property `name` of current heading or create it.
+
+If no property with `name` exists, set it to `value`.  Return `value`.
+
+If `dont-inherit` is non-nil, do not use property inheritance.
+
+### orgba-set-property `(name value)`
+
+Set property `name` of current heading to `value`.
+
+Return `value`.
+
+## drawers
+
+### orgba-get-drawer `(name &optional use-children)`
+
+Return position of drawer with `name` in current trees's content.
+
+If no such drawer exists return nil.
+
+The content of the tree is the area from the headline to the next
+headline on any level.  This function does not search the child
+subtrees unless `use-children` is non-nil.
+
+### orgba-get-drawer-create `(name &optional use-children)`
+
+Return position of drawer with `name` in current trees's content.
+
+If no such drawer exists create it in the current tree.
+
+The content of the tree is the area from the headline to the next
+headline on any level.  This function does not search the child
+subtrees unless `use-children` is non-nil.
+
+## tables
+
+### orgba-insert-table `(&rest columns)`
+
+Insert a new empty table with `columns`.
+
+### orgba-append-row `(row-data)`
+
+### orgba-lisp-to-table `(table-data)`
+
+## headings
+
+### orgba-next-heading
+
+Go to next heading or end of file if at the last heading.
+
+Return point.
+
+### orgba-next-uncle-heading
+
+Go to the first sibling of parent heading or end of file.
+
+Return point.
+
+### orgba-top-heading
+
+Go to the top parent of current heading.
+
+Return point.
+
+### orgba-heading-at `(&optional point)`
+
+Return the heading element at `point`.
+
+### orgba-heading-title-at `(&optional point)`
+
+Return the heading title at `point`.
+
+### orgba-map-headings `(fun)`
+
+Map `fun` over all the headlines in the buffer (as elements)
+
+## blocks
+
+### orgba-in-any-block-p `(&optional point)`
+
+Non-nil when point is in any org block.
+
+## search
+
+## time and dates
+
+### orgba-time-as-timestamp `(&optional time active)`
+
+Format `time` (defaults to now) as org timestamp.
+
+If `active` is non-nil, format as active timestamp.
+
+## agenda
+
+### orgba-agenda-is-task-p
+
+Return non-nil if line at point is a task.
+
+## misc
+
+### orgba-restricted-p
+
+Return non-nil if org is restricted to a subtree.
+
+# Interactive commands
+
+## headings
+
+### orgba-narrow-to-top-heading
+
+Narrow to the top-most tree containing point.
+
+## tables
+
+### orgba-table-select-cell
+
+Select the cell in org table the point is in.
---
+++ orgba.el
@@ -0,0 +1,188 @@
+;;; orgba.el --- org buffer api -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Matúš Goljer
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created:  8th September 2018
+;; Package-requires: ((dash "2.14.0"))
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Better API for org mode
+
+;;; Code:
+
+(require 'org)
+
+(defun orgba-get-property (name &optional dont-inherit)
+  "Get property NAME of current heading.
+
+If DONT-INHERIT is non-nil, do not use property inheritance."
+  (org-entry-get (point) name (not dont-inherit)))
+
+(defun orgba-get-property-create (name value &optional dont-inherit)
+  "Get property NAME of current heading or create it.
+
+If no property with NAME exists, set it to VALUE.  Return VALUE.
+
+If DONT-INHERIT is non-nil, do not use property inheritance."
+  (or (orgba-get-property name dont-inherit)
+      (orgba-set-property name value)))
+
+(defun orgba-set-property (name value)
+  "Set property NAME of current heading to VALUE.
+
+Return VALUE."
+  (org-entry-put (point) name value)
+  value)
+
+(defun orgba-get-drawer (name &optional use-children)
+  "Return position of drawer with NAME in current trees's content.
+
+If no such drawer exists return nil.
+
+The content of the tree is the area from the headline to the next
+headline on any level.  This function does not search the child
+subtrees unless USE-CHILDREN is non-nil."
+  (save-excursion
+    (org-back-to-heading)
+    (forward-line)
+    (let ((limit (or
+                  (when use-children (save-excursion (org-end-of-subtree t)))
+                  (save-excursion (re-search-forward org-heading-regexp nil t))
+                  (point-max))))
+      (catch 'done
+        (while (re-search-forward org-drawer-regexp limit t)
+          (when (equal (match-string 1) name)
+            (forward-line)
+            (throw 'done (point))))))))
+
+(defun orgba-get-drawer-create (name &optional use-children)
+  "Return position of drawer with NAME in current trees's content.
+
+If no such drawer exists create it in the current tree.
+
+The content of the tree is the area from the headline to the next
+headline on any level.  This function does not search the child
+subtrees unless USE-CHILDREN is non-nil."
+  (save-excursion
+    (-if-let (drawer-pos (orgba-get-drawer name use-children))
+        drawer-pos
+      (org-insert-property-drawer)
+      (org-back-to-heading)
+      (re-search-forward org-drawer-regexp nil t 2)
+      (org-insert-drawer nil name)
+      (point))))
+
+(defun orgba-insert-table (&rest columns)
+  "Insert a new empty table with COLUMNS."
+  (insert (format "| %s |\n|-"
+                  (mapconcat 'identity columns " | ")))
+  (org-table-align))
+
+(defun orgba-next-heading ()
+  "Go to next heading or end of file if at the last heading.
+
+Return point."
+  (or (outline-next-heading) (goto-char (point-max)))
+  (point))
+
+(defun orgba-next-parent-sibling ()
+  "Go to the first sibling of parent heading or end of file.
+
+Return point."
+  (condition-case err
+      (progn
+        (outline-up-heading 1)
+        (outline-get-next-sibling))
+    (error
+     (goto-char (point-max)))))
+
+(defun orgba-top-parent ()
+  "Go to the top parent of current heading."
+  (interactive)
+  (while (org-up-heading-safe)))
+
+(defun orgba-heading-at (&optional point)
+  "Return the heading element at POINT."
+  (save-excursion
+    (goto-char (or point (point)))
+    (org-back-to-heading)
+    (cadr (org-element-at-point))))
+
+(defun orgba-heading-title-at (&optional point)
+  "Return the heading element at POINT."
+  (plist-get (orgba-heading-at point) :title))
+
+(defun orgba-map-headings (fun)
+  "Map FUN over all the headings in the buffer (as elements)"
+  (org-element-map (org-element-parse-buffer 'headline) 'headline fun))
+
+(defun orgba-in-any-block-p ()
+  "Non-nil when point is in any org block."
+  (save-match-data
+    (let ((case-fold-search t)
+          (lim-up (save-excursion (outline-previous-heading)))
+          (lim-down (save-excursion (outline-next-heading))))
+      (org-between-regexps-p "^[ \t]*#\\+begin_" "^[ \t]*#\\+end_" lim-up lim-down))))
+
+(defun orgba-time-as-timestamp (&optional time active)
+  "Format TIME (defaults to now) as org timestamp.
+
+If ACTIVE is non-nil, format as active timestamp."
+  (with-temp-buffer
+    (org-insert-time-stamp
+     (or time (org-current-time org-clock-rounding-minutes))
+                           'with-hm (not active))
+    (buffer-string)))
+
+(defun orgba-agenda-is-task-p ()
+  "Return non-nil if line at point is a task."
+  (org-get-at-bol 'org-marker))
+
+(defun orgba-restricted-p ()
+  "Return non-nil if org is restricted to a subtree."
+  (marker-buffer org-agenda-restrict-begin))
+
+(defun orgba-narrow-to-top-heading ()
+  "Narrow to the top-most tree containing point."
+  (interactive)
+  (save-excursion
+    (ignore-errors (while (outline-up-heading 1)))
+    (org-narrow-to-subtree)))
+
+(defun orgba-table-select-cell ()
+  "Select the cell in org table the point is in."
+  (interactive)
+  (when (org-table-p)
+    (let ((b (save-excursion
+               (re-search-forward "|")
+               (backward-char 1)
+               (skip-chars-backward " ")
+               (point)))
+          (e (save-excursion
+               (re-search-backward "|")
+               (forward-char 1)
+               (skip-chars-forward " ")
+               (point))))
+      (push-mark b t t)
+      (goto-char e))))
+
+(provide 'orgba)
+;;; orgba.el ends here
